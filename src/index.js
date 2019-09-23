5929496e462a92739b117d8f7abc73d049fc20f4
Ambrance <ambrance@mail.ru>
2019-09-23 13:22:10 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configStr = bracketsConfig.toString().split(',').join('');
+  let configArr = [];
+  for (let i = 0; i < configStr.length; i += 2) {
+    configArr.push(configStr.slice(i, i + 2));
+  }
+  while (configArr.some(elem => str.includes(elem))) {
+    for (let k = 0; k < configArr.length; k++) {
+      if (str.includes(configArr[k])) {
+        str = str.replace(configArr[k], '');
+      }
+    }
+  }
+  if (str.length !== 0) {
+    return false;
+  }
+  return true;
 }
